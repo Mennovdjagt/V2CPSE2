439173164c2427acd65db74a58ed129e101ef593
mennovdjagt <menno.vanderjagt@student.hu.nl>
2019-12-07 14:22:45 +0100

    counting isalpha fixed

--- Opdr4/main.cpp
+++ Opdr4/main.cpp
@@ -7,15 +7,18 @@
 int main( int argc, char *argv[] ){
 
   std::string word;
-  std::vector<char*> tekst;
+  std::vector<char> tekst;
 
   std::ifstream myFile("tekst.txt");
 
   while(myFile >> word){
       for(unsigned int i = 0; i < word.length(); i++){
-          tekst.push_back(&word[i]);
+          tekst.push_back(word[i]);
+          std::cout << word[i] << std::endl;
       }
   }
+
+  std::cout << std::endl;
 
   std::cout << "aantal characters: " << tekst.size() << std::endl;
 
@@ -23,5 +26,8 @@ int main( int argc, char *argv[] ){
 
   std::cout << "aantal regels: " << std::count(std::istreambuf_iterator<char>(lines), 
                      std::istreambuf_iterator<char>(), '\n') + 1 << std::endl;
+
+  std::cout << std::count_if(tekst.begin(), tekst.end(), [](char c){ return std::isalpha(c); }) << std::endl;
+  std::cout << std::count_if(tekst.begin(), tekst.end(), [](char c){ return std::isdigit(c); }) << std::endl;
 
 }
