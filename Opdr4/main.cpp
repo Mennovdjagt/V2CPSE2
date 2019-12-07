52b86c6e122c19edf9d64c21de6ce3204bcfb2bf
mennovdjagt <menno.vanderjagt@student.hu.nl>
2019-12-07 16:38:33 +0100

    map integrated

--- Opdr4/main.cpp
+++ Opdr4/main.cpp
@@ -4,12 +4,13 @@
 #include <fstream>
 #include <algorithm>
 #include <array>
+#include <map>
 
 int main( int argc, char *argv[] ){
 
-  std::string word;                           //buffer to save a word temporary
-  std::vector<char> tekst;                    //dynamic array to save all the characters out of the file in
-  std::array<unsigned int, 10> list;          //to save the amount of digits that will be counted (on index)
+  std::string word;                               //buffer to save a word temporary
+  std::vector<char> tekst;                        //dynamic array to save all the characters out of the file in
+  std::map<unsigned int, unsigned int> digits;    //to save the amount of digits that will be counted (on key)     
 
   std::ifstream myFile( "tekst.txt" );
 
@@ -26,28 +27,30 @@ int main( int argc, char *argv[] ){
   std::cout << "aantal regels: \t\t" << std::count(std::istreambuf_iterator<char>(lines), 
                      std::istreambuf_iterator<char>(), '\n') + 1 << std::endl;                                        //counts all lines
 
-  std::cout << "aantal letters: \t" << std::count_if(tekst.begin(), tekst.end(), [](char c){ return std::isalpha(c); }) << std::endl;       //counts the characters if they are alpha
-  std::cout << "aantal cijfers: \t" << std::count_if(tekst.begin(), tekst.end(), [](char c){ return std::isdigit(c); }) << std::endl;       //counts all the digits
+  std::cout << "aantal letters: \t" << std::count_if(tekst.begin(), tekst.end(), [](char c){ return std::isalpha(c); }) << std::endl;               //counts the characters if they are alpha
+  std::cout << "aantal cijfers: \t" << std::count_if(tekst.begin(), tekst.end(), [](char c){ return std::isdigit(c); }) << "\n" << std::endl;       //counts all the digits
 
-  for_each(tekst.begin(), tekst.end(), [](char&c){ if(std::isdigit(c)){ c = ((int)c + 49); } });                      //changes all the digits to characters
+  //for_each(tekst.begin(), tekst.end(), [](char&c){ if(std::isdigit(c)){ c = ((int)c + 49); } });                      //changes all the digits to characters
 
-  for( unsigned int i = 0; i < tekst.size(); i++ ){   //prints the newly changed vector (should be without the digits)
+  for( unsigned int i = 0; i < tekst.size(); i++ ){                              //prints the newly changed vector (should be without the digits)
       std::cout << tekst.at(i);
   }
 
-  std::cout << std::endl;
+  std::cout << "\n\n";
 
-  std::for_each(list.begin(), list.end(), [](unsigned int&x){ x = 0; });              
+  for(uint i = 0; i < 11; i++){                                                 //fills the map with 0's
+      digits.insert(std::pair<char,int>( i, 0 ) );
+  }            
 
-  for( auto p : tekst ){                            //loops through all the characters looking for digits to count
-      if( std::isdigit(p) ){          
-          unsigned int x = list[(int)(p-'0')];      // -'0' is because the char for 0-9 is a different number in the ASCII table
-          list[(int)(p-'0')] = x + 1;               // adds 1 to the number(same at index)
+  for( auto p : tekst ){                                                        //loops through all the characters looking for digits to count
+      if( std::isdigit(p) ){         
+          unsigned int x = digits.at((int)(p-'0'));                             // -'0' is because the char for 0-9 is a different number in the ASCII table
+          digits.at((int)(p-'0')) = x;
       }
   }
 
-  for(unsigned int i = 0; i < 10; i++){             //prints all the digits from 0 to 9 with the corresponding amount behind it
-    std::cout << i << ":" << list[i] << ", ";
+  for(auto &p : digits){                                                        //print all the numbers with there corresponding amount (not sorted yet)
+      std::cout << p.first << ":" << p.second << std::endl;
   }
 
 
