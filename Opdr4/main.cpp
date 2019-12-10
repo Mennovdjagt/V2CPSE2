98aba6bb77bb1bfade649cfcfff2b677621af464
mennovdjagt <menno.vanderjagt@student.hu.nl>
2019-12-10 13:36:42 +0100

    counting uppercase working

--- Opdr4/main.cpp
+++ Opdr4/main.cpp
@@ -17,6 +17,7 @@ int main( int argc, char *argv[] ){
       for( unsigned int i = 0; i < word.length(); i++ ){            //loops through the length of the word and pushes every character into the vector
           tekst.push_back( word[i] );
       }
+      tekst.push_back(' ');
   }
 
   std::cout << "aantal characters: \t" << tekst.size() << std::endl;                                                  //reads the size using 
@@ -26,10 +27,13 @@ int main( int argc, char *argv[] ){
   std::cout << "aantal regels: \t\t" << std::count(std::istreambuf_iterator<char>(lines), 
                      std::istreambuf_iterator<char>(), '\n') + 1 << std::endl;                                        //counts all lines
 
-  std::cout << "aantal letters: \t" << std::count_if(tekst.begin(), tekst.end(), [](char c){ return std::isalpha(c); }) << std::endl;               //counts the characters if they are alpha
-  std::cout << "aantal cijfers: \t" << std::count_if(tekst.begin(), tekst.end(), [](char c){ return std::isdigit(c); }) << "\n" << std::endl;       //counts all the digits
+  std::cout << "aantal letters: \t" << std::count_if(tekst.begin(), tekst.end(), [](char c){ return std::isalpha(c); }) << std::endl;                     //counts the characters if they are alpha
+  std::cout << "aantal hoofdletters: \t" << std::count_if(tekst.begin(), tekst.end(), [](char c){ return std::isupper(c); }) << std::endl;        //counts the characters if they are uppercase
+  std::cout << "aantal cijfers: \t" << std::count_if(tekst.begin(), tekst.end(), [](char c){ return std::isdigit(c); }) << "\n" << std::endl;             //counts all the digits
 
-  //for_each(tekst.begin(), tekst.end(), [](char&c){ if(std::isdigit(c)){ c = ((int)c + 49); } });                      //changes all the digits to characters
+  for_each(tekst.begin(), tekst.end(), [](char&c){ if(std::isdigit(c)){ c = ((int)c + 49); } });                      //changes all the digits to characters
+
+  for_each(tekst.begin(), tekst.end(), [](char&c){ if(std::isupper(c)){ c = std::tolower(c); } }); 
 
   for( uint i = 0; i < tekst.size(); i++ ){                                     //prints the newly changed vector (should be without the digits)
       std::cout << tekst.at(i);
